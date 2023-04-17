23fb6c68971dd9902bbe86e4e674c466b18c7c2f
Daniel Fritzson <daniel.fritzson@yahoo.com>
2023-04-16 23:02:23 -0700

    sessions

--- digits_app/app.py
+++ digits_app/app.py
@@ -4,7 +4,7 @@ import sys
 path = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(path)
 
-from flask import Blueprint, Flask, redirect, render_template, request, url_for
+from flask import Blueprint, Flask, redirect, render_template, request, session, url_for
 from gevent.pywsgi import WSGIServer
 
 from digits_app.forms import DigitsForm, GuessForm
@@ -16,6 +16,8 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "my-secret-key"
 digits_bp = Blueprint("digits_bp", __name__)
 
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
 
 digits_obj = Digits()
 
@@ -24,37 +26,40 @@ digits_obj = Digits()
 def digits():
     form = DigitsForm()
     if form.validate_on_submit():
-        digits_obj.difficulty = form.difficulty.data
-        digits_obj.num_digits = form.num_digits.data
-        digits_obj.answer = digits_obj.generate_answer(digits_obj.num_digits)
+        session["difficulty"] = form.difficulty.data
+        session["num_digits"] = form.num_digits.data
+        session["answer"] = digits_obj.generate_answer(session["num_digits"])
         return redirect(url_for("digits_bp.guess"))
     return render_template("digits.html", form=form)
 
 
 @digits_bp.route("/guess", methods=["GET", "POST"])
 def guess():
-    guess_form = GuessForm(digits_obj.num_digits, request.form)
-    clue_gen = ClueGenerator(digits_obj.answer, digits_obj.difficulty)
+    num_digits = session.get("num_digits", 5)
+    difficulty = session.get("difficulty", "easy")
+    answer = session.get("answer", 0)
+    guess_form = GuessForm(num_digits, request.form)
+    clue_gen = ClueGenerator(answer, difficulty)
     solver = Solver(
         clue_gen.digits,
         clue_gen.num_maps,
         clue_gen.clues,
-        digits_obj.difficulty,
+        difficulty,
         digits_obj.map_files,
     )
-    digits_obj.clues = solver.final_clues
+    clues = solver.final_clues
     str_clues = []
-    for clues in digits_obj.clues.values():
+    for clues in clues.values():
         for clue in clues:
             str_clues.append(str(clue))
 
     guess = None
-    number = digits_obj.answer
+    number = answer
     if guess_form.validate_on_submit():
         if "submit_guess" in request.form:
             guess_digits = [
                 str(getattr(guess_form, f"digit_{digit}").data)
-                for digit in range(1, digits_obj.num_digits + 1)
+                for digit in range(1, num_digits + 1)
             ]
             guess = int("".join(guess_digits))
         elif "submit_back" in request.form:
@@ -65,12 +70,12 @@ def guess():
         clues=str_clues,
         guess=guess,
         number=number,
-        digits_obj=digits_obj,
     )
 
 
 app.register_blueprint(digits_bp)
 
-# if __name__ == "__main__":
-#     http_server = WSGIServer(('', 5000), app)
-#     http_server.serve_forever()
+if __name__ == "__main__":
+    # http_server = WSGIServer(('', 5000), app)
+    # http_server.serve_forever()
+    app.run(debug=True)
